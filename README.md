cb58ba01c6cb7f9a78ac574b4055f463083513d4
esp-game <masloff@zoho.com>
2020-07-24 16:49:41 +0500

    Upgrade

--- README.md
+++ README.md
@@ -21,16 +21,19 @@ cd OpenCV-Webcam-Recorder-and-Streamer
 Install Python libraries
 
 ```
-pip3 install opencv-python
-pip3 install numpy
-pip3 install flask
-pip3 install imutils 
+./install.sh
 ```
 
-Start ```main.py```
+Start ```main.py``` or ```start.sh```
 
 ```
 python3 main.py
+```
+
+or 
+
+```
+./start.sh
 ```
 
 ## Examples
---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install libatlas-base-dev -y
+sudo apt-get install libjasper-dev -y
+sudo apt-get install libqtgui4 -y
+sudo apt-get install python3-pyqt5 -y
+sudo apt-get install libqt4-test -y
+sudo apt-get install libhdf5-dev -y
+sudo apt-get install libhdf5-serial-dev -y
+pip3 install opencv-python
+pip3 install opencv-contrib-python==4.1.0.25
+pip3 install numpy
+pip3 install flask
+pip3 install imutils
+pip3 install zmq
